170aec0966889159aee3a130a71977b08a9ced68
M6F\student <wlgns679@gmail.com>
2025-03-28 17:53:39 +0900

    feat: slot 연습문제 (미완)

---
+++ 0328_vue(컴포넌트_심화)/실습-example/src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import NoSlotTest from './components/NoSlotTest.vue';
+</script>
+
+<template>
+  <div>
+    <NoSlotTest />
+  </div>
+</template>
+
+<style scoped></style>
---
+++ 0328_vue(컴포넌트_심화)/실습-example/src/components/CheckBox1.vue
@@ -0,0 +1,33 @@
+<script setup>
+const emits = defineEmits(['check-changed']);
+const props = defineProps({
+  id: String,
+  checked: Boolean,
+  label: String,
+});
+
+const checkChange = (e) => {
+  const newChecked = e.target.checked;
+  emits('check-changed', { id: props.id, checked: newChecked });
+};
+</script>
+
+<template>
+  <div>
+    <input
+      type="checkbox"
+      :value="id"
+      :checked="checked"
+      @change="checkChange"
+    />
+    <span
+      v-if="checked === true"
+      style="color: blue; text-decoration: underline"
+    >
+      <i>{{ label }}</i>
+    </span>
+    <span v-else style="color: gray">{{ label }}</span>
+  </div>
+</template>
+
+<style scoped></style>
